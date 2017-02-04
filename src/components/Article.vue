245b0b211464a81184a4f2009f079c85a6379fc3
DemonGao <a750229099@qq.com>
2017-02-04 17:01:38 +0800

    增加 文章细节栏 日期、作者、阅读量、分类

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -2,8 +2,22 @@
 	<div class="article">
 		<h1 class="article-head clearfix">
 			{{data.title}}
-			<span class="article-date">{{new Date(data.date).Format("YYYY-MM-DD")}}</span>
+			<!--<span class="article-date">{{new Date(data.date).Format("YYYY-MM-DD")}}</span>-->
 		</h1>
+    <div class="article-detail">
+      <span class="article-detail-meta">
+        <i class="iconfont icon-tubiaozitihua02"></i> {{new Date(data.date).Format("YYYY-MM-DD")}}
+      </span>
+      <span class="article-detail-meta">
+        <i class="iconfont icon-liulan"></i>阅读: {{data.view}}
+      </span>
+      <span class="article-detail-meta">
+        <i class="iconfont icon-zuozhe"></i>作者: DemonGao(高世超)
+      </span>
+      <span class="article-detail-meta">
+        <i class="iconfont icon-biaoqian"></i>分类: <router-link :to="{name:'home',params:{tag:data.tag}}" class="sign-item">{{data.tag}}</router-link>
+      </span>
+    </div>
 		<div v-html="data.content">
 
 		</div>
@@ -59,6 +73,18 @@
 		font-size: 12px;
 		line-height: 39px
 	}
+  .article-detail{
+    padding:15px 20px;
+  }
+  .article-detail .article-detail-meta{
+    margin-right:20px;
+  }
+  .article-detail .article-detail-meta:last-child{
+    margin-right: 0;
+  }
+  .article-detail .article-detail-meta i{
+    margin-right:3px;
+  }
   @media (max-width: 768px) {
     .article{
       padding:10px;
